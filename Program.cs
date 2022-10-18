ac350b4aae1800fb1fb61d62e75f7519ff7ed7a5
Isaiah-Ferguson <crestice@yahoo.com>
2022-10-18 11:39:29 -0700

    Ready for Review

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿//Isaiah Keith Ferguson
 //10/17/2022
 //Mad Lib
+//The user will enter a 
 
 
 bool truenum = true;
-string Sport, reTry, Adjective, Size, Emotion, Food, Restaurant, celebrity, animeCharacter, shoe, num;
-int realnum;
+string Sport, reTry, Adjective, Size, Emotion, Food, Restaurant, celebrity, animeCharacter, shoe, num, num1;
+int realnum, realnum2, trunum3;
 reTry = "";
 
 while(reTry != "end")
@@ -15,8 +16,11 @@ while(reTry != "end")
 Console.WriteLine("Enter any number");
 num = Console.ReadLine();
 truenum = Int32.TryParse(num, out realnum);
+Console.WriteLine("Enter any number");
+num1 = Console.ReadLine();
+bool truenum1 = Int32.TryParse(num1, out realnum2);
 
-if (!truenum)
+if (!truenum || !truenum1)
 {
     Console.WriteLine("Please retry and enter a real number next time");
 }else
@@ -48,15 +52,18 @@ animeCharacter = Console.ReadLine();
 Console.WriteLine("Enter your favorite Shoes");
 shoe = Console.ReadLine();
 
+trunum3 = realnum + realnum2;
+
 Console.WriteLine($"{Sport} Is the best {Adjective} Anime ever!");
 Console.WriteLine($"the main character's feet seems kinda {Size}");
 Console.WriteLine($"I can't believed they named him after {celebrity}");
 Console.WriteLine($"The plot had so many twists i can't believe {animeCharacter} Showed up at the end!");
 Console.WriteLine($"did you see when {celebrity} Wore {shoe}'s I really wish he didn't wear those");
 Console.WriteLine($"Honestly it made me kind of {Emotion}");
-Console.WriteLine($"Anyway, I think this anime deserves {realnum} Stars!");
+Console.WriteLine($"Anyway, {celebrity} thought that {realnum} + {realnum2} was 21 instead of {trunum3} ! what a memer!!");
 Console.WriteLine($"I usually watch it at {Restaurant} I go there so often they usually give me free {Food}");
 }
+Console.WriteLine("");
 Console.WriteLine($"Type {reTry} Please Type 'End' to stop talking to the computer or any other key input to restart");
 reTry = Console.ReadLine().ToLower();
-    }
+}
